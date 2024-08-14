e32fc9b41cc5d5ea4990e8ed608f20e62b10345d
Alex Skrenchuk <alexskr@stanford.edu>
2024-08-14 13:19:33 -0700

    rename rrestart to rolling-restart for clairty

--- manifests/rolling_restart.pp
+++ manifests/rolling_restart.pp
@@ -10,34 +10,35 @@
 # Requires:
 #
 # Sample Usage:
-class mgrep::rrestart (
+class mgrep::rolling_restart (
   Stdlib::Host $redis_host = 'localhost',
   StdLib::Port $redis_port = 6379,
-  Boolean $mgrep_primary   = false,
+  Boolean $primary_node    = false,
   String $cron_string      = '*/5 * * * *',
   Boolean $verbose         = true,
 ) {
-
   ensure_packages(['python3-redis'])
 
-  file { '/opt/mgrep/bin/rrestart':
+  file { '/opt/mgrep/bin/rolling-restart':
     mode   => '0755',
     owner  => root,
     group  => root,
-    source => 'puppet:///modules/mgrep/rrestart',
+    source => 'puppet:///modules/mgrep/rolling-restart',
   }
+
   if $verbose {
     $verbose_flag = '-v >> /var/log/mgreprestart 2>&1'
   } else {
     $verbose_flag = '>> /dev/null 2>&2'
   }
-  if ( $mgrep_primary == true) {
+
+  if ( $primary_node == true) {
     $cron_content = "${cron_string} root /opt/mgrep/bin/rrestart -m -r ${redis_host} -p ${redis_port} ${verbose_flag} \n"
   } else {
     $cron_content = "${cron_string} root /opt/mgrep/bin/rrestart -r ${redis_host} -p ${redis_port} ${verbose_flag} \n"
   }
 
-  file { '/etc/cron.d/mgrep-rrestart':
+  file { '/etc/cron.d/mgrep-rolling-restart':
     mode    => '0644',
     content => $cron_content,
   }
